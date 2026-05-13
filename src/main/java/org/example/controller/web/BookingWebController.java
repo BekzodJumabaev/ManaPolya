df2a7e8313c4703feff89d6dc48258ccf9728837
BekzodJumabaev <bekzodjumabaev04@gmail.com>
2026-05-13 18:02:44 +0500

    Kengayib, xatolar to'g'irlanayapti

---
+++ src/main/java/org/example/controller/web/BookingWebController.java
@@ -0,0 +1,40 @@
+package org.example.controller.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.BookingCreateDto;
+import org.example.service.BookingService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class BookingWebController {
+
+    private final BookingService bookingService;
+
+    @PostMapping("/fields/{id}/book")
+
+    public String bookField(
+            @PathVariable("id") Long id,
+            @ModelAttribute BookingCreateDto dto,
+            Principal principal,
+            RedirectAttributes redirectAttributes
+            ){
+       try {
+           dto.setFieldId(id);
+           bookingService.createBooking(dto, principal.getName());
+           redirectAttributes.addFlashAttribute("message", "Maydon muvaffaqiyatli bron qilindi: ");
+           return "redirect:/fields/" + id;
+       }catch (Exception e){
+           redirectAttributes.addFlashAttribute("error", e.getMessage());
+           return "redirect:/fields/" + id;
+       }
+    }
+}
